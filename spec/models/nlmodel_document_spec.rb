dc8dade4dc786a690621c97cd432c9f176fc7ed2
reuven <reuven@22fd05b1-1c41-0410-b59c-fbdc84b6a882>
2008-12-12 09:41:53 +0000

    Removed old spec
    git-svn-id: svn+ssh://monk.ccl.northwestern.edu/srv/svnrepos/ccl/nlcommons/trunk@178 22fd05b1-1c41-0410-b59c-fbdc84b6a882

--- spec/models/nlmodel_document_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe NlmodelDocument do
-
-  it "should be associated with a model"
-  it "should be associated with a person"
-  it "should have a title"
-
-end
