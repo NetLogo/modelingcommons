434a1ac662fd1169199f57665ee3492bc7e3616f
reuven <reuven@22fd05b1-1c41-0410-b59c-fbdc84b6a882>
2009-04-26 19:51:09 +0000

    Updated deploy.rb
    git-svn-id: svn+ssh://monk.ccl.northwestern.edu/srv/svnrepos/ccl/nlcommons/trunk@360 22fd05b1-1c41-0410-b59c-fbdc84b6a882

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,27 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "nlcommons"
+set :repository,  "svn+ssh://monk.ccl.northwestern.edu/srv/svnrepos/ccl/nlcommons/trunk"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/www.modelingcommons.org/www/trunk"
+set :deploy_via, :export
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :scm_username, 'reuven'
+set :scm_password, proc{ Capistrano::CLI.password_prompt('Enter password:')}
+set :use_sudo, false
+
+role :app, "main.lerner.co.il"
+role :web, "main.lerner.co.il"
+role :db, "main.lerner.co.il", :primary => true
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
