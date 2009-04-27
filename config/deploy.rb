57c9aa63ec967c44017c639af98c58936d23ecde
Reuven Lerner <reuven@lerner.co.il>
2009-04-26 22:36:04 -0500

    Updated deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,18 @@
-set :application, "nlcommons"
-set :repository,  "svn+ssh://monk.ccl.northwestern.edu/srv/svnrepos/ccl/nlcommons/trunk"
+set :application, "NetLogo Modeling Commons"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/var/www/www.modelingcommons.org/www/trunk"
-set :deploy_via, :export
+default_run_options[:pty] = true
+set :repository,  "git@lerner.co.il:/home/git/nlcommons.git"
+set :scm, "git"
+set :scm_passphrase, "" #This is your custom users password
+set :user, "deploy"
+set :branch, 'master'
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
-
-set :scm_username, 'reuven'
-set :scm_password, proc{ Capistrano::CLI.password_prompt('Enter password:')}
-set :use_sudo, false
-
+role :db, "main.lerner.co.il"
 role :app, "main.lerner.co.il"
 role :web, "main.lerner.co.il"
-role :db, "main.lerner.co.il", :primary => true
+
+set :deploy_to, "/var/www/www.modelingcommons.org/www/"
+set :deploy_via, :export
 
 namespace :deploy do
   desc "Restart Application"
