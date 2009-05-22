d11d21491be99c238aa8f0fe40b8e9372ce1eaf9
Reuven Lerner <reuven@lerner.co.il>
2009-05-22 10:59:17 +0300

    Updated to reflect hostnames
    Former-commit-id: f61bc904777598d833dcb85f8959a4aceb077cbc

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,15 @@
 set :application, "NetLogo Modeling Commons"
 
 default_run_options[:pty] = true
-set :repository,  "git@lerner.co.il:/home/git/nlcommons.git"
+set :repository,  "git@main.lerner.co.il:/home/git/nlcommons.git"
 set :scm, "git"
 set :scm_passphrase, "" #This is your custom users password
 set :user, "deploy"
 set :branch, 'master'
 
-role :db, "lerner.co.il"
-role :app, "lerner.co.il"
-role :web, "lerner.co.il"
+role :db, "main.lerner.co.il"
+role :app, "main.lerner.co.il"
+role :web, "main.lerner.co.il"
 
 set :deploy_to, "/var/www/www.modelingcommons.org/www/"
 set :deploy_via, :export
