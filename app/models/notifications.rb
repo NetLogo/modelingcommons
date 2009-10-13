4a4478da7e5f77fa7d993d8b1c22c5a292032999
Reuven Lerner <reuven@lerner.co.il>
2009-10-13 09:47:30 +0200

    Fixed mailer problems
    Former-commit-id: 55db0a513cc7ccacfccf37bf2c385673912c5cfd

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -8,98 +8,100 @@ class Notifications < ActionMailer::Base
     :enable_starttls_auto => false
   }
 
+  FROM_ADDRESS = 'nlcommons@modelingcommons.org'
+
   def signup(person)
     @recipients = person.email_address
     @bcc = 'reuven@lerner.co.il'
-    @from = "nlcommons@monk.ccl.northwestern.edu"
+    @from = FROM_ADDRESS
     @subject = "Welcome to the NetLogo Modeling Commons!"
     @body[:person] = person
-    @sent_on    = Time.now()
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
   def reset_password(person)
     @recipients = person.email_address
-    @from = "nlcommons@monk.ccl.northwestern.edu"
+    @from = FROM_ADDRESS
     @body[:person] = person
-    @subject    = 'Your new password'
-    @sent_on    = Time.now()
+    @subject = 'Your new password'
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
   def password_reminder(person)
     @recipients = person.email_address
-    @from = "nlcommons@monk.ccl.northwestern.edu"
+    @from = FROM_ADDRESS
     @body[:person] = person
-    @subject    = 'Your password'
-    @sent_on    = Time.now()
+    @subject = 'Your password'
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
   def changed_password(person)
     @recipients = person.email_address
-    @from = "nlcommons@monk.ccl.northwestern.edu"
+    @from = FROM_ADDRESS
     @body[:person] = person
-    @subject    = 'Your new password'
-    @sent_on    = Time.now()
+    @subject = 'Your new password'
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
   def modified_model(people, node)
     @recipients = node.people
-    @from = "nlcommons@monk.ccl.northwestern.edu"
-    @subject    = 'Model update'
+    @from = FROM_ADDRESS
+    @subject = 'Model update'
     @body[:nlmodel] = node
-    @sent_on    = Time.now()
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
   def applied_tag(people, tag)
     @recipients = people.map{|p| p.email_address}
-    @from = "nlcommons@monk.ccl.northwestern.edu"
-    @subject    = 'Tag was applied'
+    @from = FROM_ADDRESS
+    @subject = 'Tag was applied'
     @body[:tag] = tag
-    @sent_on    = Time.now()
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
   def updated_discussion(people, nlmodel)
     @recipients = people.map{|p| p.email_address}
-    @from = "nlcommons@monk.ccl.northwestern.edu"
-    @subject    = 'Updated discussion'
+    @from = FROM_ADDRESS
+    @subject = 'Updated discussion'
     @body[:nlmodel] = nlmodel
-    @sent_on    = Time.now()
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
   def invited_to_group(person, membership)
     @recipients = person.email_address
-    @from = "nlcommons@monk.ccl.northwestern.edu"
-    @subject    = 'Group invitation from the NetLogo Modeling Commons'
+    @from = FROM_ADDRESS
+    @subject = 'Group invitation from the NetLogo Modeling Commons'
     @body[:membership] = membership
-    @sent_on    = Time.now()
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
   def friend_recommendation(sender, friend_email_address, node)
     @recipients = friend_email_address
     @cc = sender.email_address
-    @from = "nlcommons@monk.ccl.northwestern.edu"
-    @subject    = 'View an interesting NetLogo model'
+    @from = FROM_ADDRESS
+    @subject = 'View an interesting NetLogo model'
     @body[:node] = node
     @body[:sender] = sender
-    @sent_on    = Time.now()
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
   def recommended_message(recommender, people, model)
     @recipients = recommender.email_address
     @bcc = people.map{|p| p.email_address}
-    @from = "nlcommons@monk.ccl.northwestern.edu"
-    @subject    = "Recommendation for the '#{model.name}' model in the NetLogo Modeling Commons"
+    @from = FROM_ADDRESS
+    @subject = "Recommendation for the '#{model.name}' model in the NetLogo Modeling Commons"
     @body[:nlmodel] = model
     @body[:recommender] = recommender
-    @sent_on    = Time.now()
+    @sent_on = Time.now()
     @content_type = 'text/html'
   end
 
