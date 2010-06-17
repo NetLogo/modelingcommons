0f25e1b1acf30d4ffe89d1dd187490a650a338d6
Reuven Lerner <reuven@lerner.co.il>
2010-06-17 14:25:16 +0300

    Removed special stuff in mongodb.rb
    Former-commit-id: 7d1f4e239167ecf8b94dbc195bdb5bd609dd1ea0

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,8 +1,1 @@
-# Open TCP connection to the MongoDB, and select the database
 MongoMapper.database = "modelingcommons_#{RAILS_ENV}"
-
-host = '127.0.0.1'
-port = 27017
-
-# Initialize the connection to MongoDB
-MongoMapper.connection = XGen::Mongo::Driver::Mongo.new(host, port, :auto_reconnect => true)
