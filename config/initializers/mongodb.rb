e976e02e74cab275dee9ba7d964ce58b4b0856f4
Reuven Lerner <reuven@lerner.co.il>
2010-06-17 14:15:50 +0300

    Fixed mongodb config again
    Former-commit-id: 0e58f39eb3eabac069cbed3c32f61f8553140399

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,15 +1,8 @@
-begin
-  mongo_config_file = File.join(WWW_ROOT, "config/mongo.yml")
-  mongo_config = YAML.load_file(mongo_config_file)[Rails.env]
-rescue
-  raise IOError, "config/mongo.yml could not be loaded."
-end
+# Open TCP connection to the MongoDB, and select the database
+MongoMapper.database = "modelingcommons_#{RAILS_ENV}"
 
-database_name = mongo_config['database'] || (raise ArgumentError.new("No MongoDB database name was set for the environment #{Rails.env}, please check 'config/mongo.yml'."))
 host = '127.0.0.1'
 port = 27017
 
 # Initialize the connection to MongoDB
 MongoMapper.connection = XGen::Mongo::Driver::Mongo.new(host, port, :auto_reconnect => true)
-# Open TCP connection to the MongoDB, and select the database
-MongoMapper.database = "modelingcommons_#{RAILS_ENV}"
