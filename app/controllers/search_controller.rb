45c5c494be41e380a8adc376b5bb5411048790ed
Reuven Lerner <reuven@lerner.co.il>
2010-06-19 21:14:07 +0300

    Search is now fast!
    Former-commit-id: 4e16996e73a04852bdd98bfac37743ebbb39b111

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -21,11 +21,9 @@ class SearchController < ApplicationController
     @tag_match_models =
       @viewable_models.select { |model| model.tags.map { |tag| tag.name}.join(' ').downcase.index(@original_search_term)}
 
-    @info_match_models =
-      @viewable_models.select { |model| model.current_version.info_keyword_index.member?(@original_search_term)}
+    @info_match_models = @viewable_models & NodeVersion.all(:info_keyword_index => @original_search_term).map {|nv| nv.node_id}
 
-    @procedures_match_models =
-      @viewable_models.select { |model| model.current_version.procedures_keyword_index.member?(@original_search_term)}
+    @procedures_match_models = @viewable_models & NodeVersion.all(:procedures_keyword_index => @original_search_term).map {|nv| nv.node_id}
 
   end
 
