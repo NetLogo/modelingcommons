47094d04cc1e6f9e8e63060271cb09724f53947b
Reuven Lerner <reuven@lerner.co.il>
2010-06-24 16:06:04 +0300

    Made tag creation much, much nicer
    Former-commit-id: 8b74eb5973839535f68a032b1422427dca28c060

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,72 +9,31 @@ class TagsController < ApplicationController
   end
 
   def create
-    new_tags = params[:new_tag]
-    new_comments = params[:new_comment]
-
-    # Get the model that we want to tag
     @node = Node.find(params[:node_id])
 
     @new_tagged_nodes = [ ]
 
-    new_tags.each_with_index do |tag_name, i|
-      tag_name = tag_name.downcase.strip
-      comment = new_comments[i].strip
+    params[:new_tag].each_with_index do |tag_name, i|
+      next if tag_name.blank? or tag_name == 'tag name'
 
-      # Default text for the tag or comment?  Blank it out.
-      next if tag_name == 'tag name'
+      comment = params[:new_comment][i].strip
+      comment = '' if comment == '(Optional) comment about why this tag is relevant to this model'
 
-      if comment == '(Optional) comment about why this tag is relevant to this model'
-        comment = ''
-      end
+      tag = Tag.find_or_create_by_name(tag_name.downcase.strip, :person_id => @person.id)
+      tn = TaggedNode.find_or_create_by_tag_id_and_person_id_and_node_id(tag.id,
+                                                                         @person.id,
+                                                                         @node.id,
+                                                                         :comment => comment)
 
-      # Blank tag?  Ignore it.
-      next if tag_name.blank?
-
-      # If this tag exists, get it.  If not, create it
-      tag = Tag.find_by_name(tag_name)
-
-      if tag.nil?
-        tag = Tag.new(:name => tag_name, :person_id => @person.id)
-
-        next if !tag.save
-      end
-
-      # Now that we have a tag, we can create a TaggedModel -- but only
-      # if this user has not yet tagged this model with this tag.
-      tn =
-        TaggedNode.find_by_tag_id_and_person_id_and_node_id(tag.id,
-                                                            @person.id,
-                                                            @node.id)
-
-      if tn.nil?
-        logger.warn "Creating new TaggedModel, since this combination (tag ID '#{tag.id}', person ID '#{@person.id}, model ID '#{@node.id}') doesn't yet exist."
-
-        tn = TaggedNode.create(:tag_id => tag.id,
-                               :person_id => @person.id,
-                               :node_id => @node.id,
-                               :comment => comment)
+      if tn.created_at < 1.minute.ago
         @new_tagged_nodes << tn
-
-        begin
-          tn.save!
-
-          flash[:notice] = "Successfully tagged this model as '#{tag_name}'.  "
-
-          # Now send e-mail notification
-          Notifications.deliver_applied_tag(tn.node.people.reject {|person| person == @person}, tn.tag)
-
-        rescue Exception => exception
-          next
-        end
-      else
-        next
+        Notifications.deliver_applied_tag(@node.people.reject { |person| person == @person}, tn.tag)
       end
-    end
 
-    respond_to do |format|
-      format.html { redirect_to :back }
-      format.js
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
     end
   end
 
