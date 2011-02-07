d15c67691b8d676680acdc74c03ddc370a42741a
Reuven Lerner <reuven@lerner.co.il>
2011-02-07 09:42:30 +0200

    Moved the salt-adding and password-encrypting Rake task into a migration
    Former-commit-id: 0971affe0e77fa2733bc8432af27f8a40106d35f

--- db/migrate/20110204050318_add_salt_to_person.rb
+++ db/migrate/20110204050318_add_salt_to_person.rb
@@ -1,9 +1,19 @@
 class AddSaltToPerson < ActiveRecord::Migration
   def self.up
-      add_column :people, :salt, :string
+    add_column :people, :salt, :string
+
+    Person.transaction do 
+      Person.all.each do |person|
+        STDERR.puts "Now updating person '#{person.fullname}'"
+        salt = Person.generate_salt(person.created_at)
+        person.update_attributes!(:salt => salt, :password => person.password)
+        STDERR.puts "\tDone."
+      end
+    end
+
   end
 
   def self.down
-      remove_column :people, :salt
+    remove_column :people, :salt
   end
 end
