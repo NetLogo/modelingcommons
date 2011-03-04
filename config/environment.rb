72422c0f701e67c86109462ccf7a729d52ff6552
Reuven Lerner <reuven@lerner.co.il>
2011-03-04 13:43:30 +0200

    use a number
    Former-commit-id: 9f5252a0448e328fd3cd36b53c0891f474f12afd

--- config/environment.rb
+++ config/environment.rb
@@ -48,7 +48,7 @@ Rails::Initializer.run do |config|
   config.action_controller.session = {
     :key => '_nlcommons_session',
     :secret      => 'efb755e46296661a3606b089381576ded95b9c8ae7697770963a5782b85fa009d0f98f8a2856d8b334d1224790e7cda374c7cc4c6f7058bade4b00f667175ae3',
-    :expire_after => 1.month
+    :expire_after => 2592000
   }
 
   # Use the database for sessions instead of the cookie-based default,
