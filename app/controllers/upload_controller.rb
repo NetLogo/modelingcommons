01e516474e1fa8cf0507a4516198085e38268392
Reuven Lerner <reuven@lerner.co.il>
2013-03-13 07:45:13 -0500

    Brought back deleting models from the dead
    Former-commit-id: 34fc4f749d5640d0bd3c8e350acccbd20fbc932c

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -62,28 +62,28 @@ class UploadController < ApplicationController
         # If we got a group and permission settings, set those as well
         group_id = params[:group_id].blank? ? nil : params[:group_id]
         group = Group.group_or_nil(group_id)
-  
+        
         read_permission = PermissionSetting.find_by_short_form(params[:read_permission])
         if group.nil? and read_permission and read_permission.is_group?
           read_permission = PermissionSetting.find_by_short_form('u')
         end
-  
+        
         write_permission = PermissionSetting.find_by_short_form(params[:write_permission])
         if group.nil? and write_permission and write_permission.is_group?
           write_permission = PermissionSetting.find_by_short_form('u')
         end
-  
+        
         @model.update_attributes(:visibility => read_permission,
                                  :changeability => write_permission,
                                  :group => group)
-                                 
+        
         
         # ------------------------------------------------------------
         # Preview image
         # ------------------------------------------------------------
-  
+        
         if params[:new_model][:uploaded_preview].present?
-  
+          
           # Create a preview
           attachment = NodeAttachment.new(:node_id => @model.id,
                                           :person_id => @person.id,
@@ -91,9 +91,9 @@ class UploadController < ApplicationController
                                           :filename => model_name + '.png',
                                           :type => 'preview',
                                           :contents => params[:new_model][:uploaded_preview].read)
-  
+          
           expire_page :action => :display_preview, :id => @model.id
-  
+          
           if !attachment.save
             response[:status] = 'SUCCESS_PREVIEW_NOT_SAVED'
             flash[:notice] = "Error creating a new preview object; it was not saved."
@@ -129,7 +129,7 @@ class UploadController < ApplicationController
 
   def update_model
     existing_node = Node.find(params[:new_version][:node_id])
-  
+    
     if params[:new_version].blank? or params[:new_version][:description].blank? or params[:new_version][:uploaded_body].blank?
       respond_to do |format|
         format.html do 
@@ -220,5 +220,40 @@ class UploadController < ApplicationController
     end
   end
 
+
+  def destroy
+    @model = Node.find(params[:id])
+
+    if @model.author?(@person)
+
+      @model.attachments.each do |attachment|
+        attachment.destroy
+      end
+
+      @model.node_versions.each do |nv|
+        nv.destroy
+      end
+
+      @model.tagged_nodes.each do |tn|
+        tn.destroy
+      end
+
+      @model.postings.each do |posting|
+        posting.destroy
+      end
+
+      @model.recommendations.each do |recommendation|
+        recommendation.destroy
+      end
+
+      @model.destroy
+
+      flash[:notice] = "Model '#{@model.name}' has been deleted."
+    else
+      flash[:notice] = "You don't have permission to delete '#{@model.name}'."
+    end
+
+    redirect_to :controller => :account, :action => :mypage
+  end
 
 end
