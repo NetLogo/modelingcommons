13c6953727c0c195faffdd602a4488c151deb739
Reuven Lerner <reuven@lerner.co.il>
2013-04-02 02:59:59 +0300

    Collaboratio types are coming up in JavaScript!
    Former-commit-id: 080c8563498aee8e5d09c717428923da549509f2

--- app/controllers/collaborator_types_controller.rb
+++ app/controllers/collaborator_types_controller.rb
@@ -7,6 +7,7 @@ class CollaboratorTypesController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @collaborator_types }
+      format.json  { render :json => @collaborator_types }
     end
   end
 
