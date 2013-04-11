de0640abb78078f8dc8f00e928c108ede4bb82d0
Reuven Lerner <reuven@lerner.co.il>
2013-04-11 07:44:32 +0300

    Fixed some valiations, moved assets
    Former-commit-id: 163db2272255c3eb7c2cf41dca242851ab9773b8

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -4,6 +4,6 @@ class Recommendation < ActiveRecord::Base
   belongs_to :person
   belongs_to :node
 
-  validates :node_id, :presence => true, :uniqueness => true, :scope => :person_id
-  validates :person_id, :presence => true, :uniqueness => true, :scope => :person_id
+  validates :node_id, :presence => true, :uniqueness => { :scope => :person_id }
+  validates :person_id, :presence => true, :uniqueness => {  :scope => :person_id }
 end
