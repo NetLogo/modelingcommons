6f5ade87952c3f062f64fac58f36ee982673f80a
Ben Rafshoon <benjaminrafshoon2014@u.northwestern.edu>
2013-04-17 17:56:20 -0500

    Fixed bug for adding collaborators and improved add collaborator input validation
    Former-commit-id: db53297376ea7c4fc1df3877e25ffa52a328c5f5

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -5,36 +5,50 @@ class CollaborationsController < ApplicationController
   before_filter :require_login
 
   def create
-    @node = Node.find(params[:node_id])
-
-    if params[:person_name].blank?
+    
+    
+    if params[:node_id].blank?
+      message = "You must provide a model to add a collaborator to"
+      
+    elsif params[:person_name].blank?
       message = "You must enter a person's name."
 
     elsif params[:collaborator_type_id].blank? 
       message = "No collaboration type indicated; ignoring."
       
-    elsif @node.author?(@person)
-      collaborator = Person.first(:conditions => ["first_name || ' ' || last_name = ?", 
-                                                  params[:person_name]])
-
-      if @node.author?(collaborator)
-        message = "Not adding '#{collaborator.fullname}', since they are already a collaborator."
-      else
-        collaboration = Collaboration.new(:node => @node,
-                                          :person => collaborator,
-                                          :collaborator_type_id => params[:collaborator_type_id])
-        if collaboration.save
-          message = "Successfully added #{collaborator.fullname} as a collaborator."
-          success = true;
-          
-        else
-          message = "Could not create the collaboration"
-        end
-      end
-
     else
-      message = 'You cannot set collaborators for this model'
+      @node = Node.find(params[:node_id])
+      if CollaboratorType.find_by_id(params[:collaborator_type_id]).nil?
+        message = "No such collaborator type"
+        
+      elsif not @node.author?(@person)
+        message = 'You cannot set collaborators for this model'
+        
+      else
+        collaborator = Person.first(:conditions => ["first_name || ' ' || last_name = ?", 
+                                                    params[:person_name]])
+  
+        if @node.author?(collaborator)
+          message = "Not adding '#{collaborator.fullname}', since they are already a collaborator."
+        else
+          
+          collaboration = Collaboration.new
+          collaboration.assign_attributes(:node => @node,
+                                            :person => collaborator,
+                                            :collaborator_type_id => params[:collaborator_type_id])
+          if collaboration.save
+            message = "Successfully added #{collaborator.fullname} as a collaborator."
+            success = true;
+            
+          else
+            message = "Could not create the collaboration"
+          end
+        end
+        
+      end
+      
     end
+    
 
     respond_to do |format| 
       format.html do
