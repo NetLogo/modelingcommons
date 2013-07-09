48835f1f8e2cd34bdc31010104898fe58b7988f9
Reuven Lerner <reuven@lerner.co.il>
2013-07-09 15:34:27 -0500

    Better messages, debugging of collaborations
    Former-commit-id: f710efbc4d35a6e8735f04a040b8ba3dedcf9946

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -7,7 +7,13 @@ class NodesController < ApplicationController
 
   def change_wants_help
     node = Node.find(params[:id])
-    node.update_attributes!(wants_help:params[:wants_help].present?)
+    logger.warn "[NodesController#change_wants_help] Checking if node #{node.id} is writable by person #{@person.inspect}"
+    render text: "No permission" unless node.changeable_by_user?(@person)
+
+    wants_help = params[:wants_help].present?
+    logger.warn "[NodesController#change_wants_help] wants_help = '#{wants_help}'"
+    node.update_attributes!(wants_help:wants_help)
+    logger.warn "[NodesController#change_wants_help] node.wants_help = '#{node.wants_help}'"
     render text: node.wants_help
   end
 
