b40fc913b95d318dc5758da0f13c4dd9efbd9561
Reuven Lerner <reuven@lerner.co.il>
2013-11-12 23:34:57 +0200

    Models, not people
    Former-commit-id: a084aed615e01b9ac1994baf48d710cf739cd5fd

--- app/models/version.rb
+++ app/models/version.rb
@@ -95,7 +95,7 @@ class Version < ActiveRecord::Base
   def tweet_version
     return unless node.world_visible?
     if node.versions.count == 1
-      Twitter.update("Added #NetLogo model '#{node.name}' at #{node.url}, our #{ActiveSupport::Inflector::ordinalize(Person.count)} model!")
+      Twitter.update("Added #NetLogo model '#{node.name}' at #{node.url}, our #{ActiveSupport::Inflector::ordinalize(Node.count)} model!")
     else
       Twitter.update("Added version #{node.versions.count} of #NetLogo model '#{node.name}': #{node.url}")
     end
