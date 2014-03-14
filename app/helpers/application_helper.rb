68c72d3afd496ec6f94b43457d9502cc3afd097d
Reuven Lerner <reuven@lerner.co.il>
2014-03-14 06:38:22 +0200

    created or updated
    Former-commit-id: 984f7029670c03a4edce99776d6923322405486f

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
     link_to_item_person = node.person
     original_node_author = node.versions.last.person
     this_user_did_it = true if original_node_author == @person
-    updated_or_created = (node.created_at.to_i == node.updated_at.to_i ? 'created' : 'updated')
+    updated_or_created = (node.updated_at.to_i >= (node.created_at.to_i + 60) ? 'created' : 'updated')
     link = url_for(:controller => "browse", :action => "one_model", :id => node.id)
     
     if !node.previews.nil? and !node.previews.empty?
