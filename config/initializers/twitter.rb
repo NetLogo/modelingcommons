381887cfc94ba11e65327a661de142e526c93168
Reuven Lerner <reuven@lerner.co.il>
2014-10-28 18:46:33 +0200

    Remove Twitter secrets.  First of many bumps in the road to open sourcing...

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,7 @@
 Twitter.configure do |config|
-  config.consumer_key = 'mg3EWK2nqZHwmoXU88xyA'
-  config.consumer_secret = 'iEdf8Sc9O7gINMTrOP57slpLBWeqxEtkhTZa4AJwn4Y'
-  config.oauth_token = '1005614226-0w6R3LY7GW7wTrG9ywDqmCy0OtK8QcJMH0oT8nQ'
-  config.oauth_token_secret = 'u9RlOy2vSf0SbO2z6BNWleiDJRCk28BH3hajBXaCD19B6'
+  config.consumer_key = ENV['twitter_consumer_key']
+  config.consumer_secret = ENV['twitter_consumer_secret']
+
+  config.consumer_key = ENV['twitter_oauth_token']
+  config.consumer_secret = ENV['twitter_oauth_token_secret']
 end
