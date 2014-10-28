1e30164f100feb9099b4debacc103ea9db30b0ce
Reuven Lerner <reuven@lerner.co.il>
2014-10-28 21:22:55 +0200

    Use environment variables, the way grown ups do it today

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,7 +1,7 @@
 Twitter.configure do |config|
-  config.consumer_key = ENV['twitter_consumer_key']
-  config.consumer_secret = ENV['twitter_consumer_secret']
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
 
-  config.consumer_key = ENV['twitter_oauth_token']
-  config.consumer_secret = ENV['twitter_oauth_token_secret']
+  config.oauth_key = ENV['TWITTER_OAUTH_TOKEN']
+  config.oauth_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
 end
